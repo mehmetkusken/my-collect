9da6b845b76f05c4349181f996707073363e2545
Mehmet <mehmetkusken@gmail.com>
2021-02-09 08:23:43 -0700

    commit tree 0af3a0da046404c4be43efbeffc0b5880433e461

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
 
+def my_collect(array)
+    i=0
+    collection=[]
+    while i<array.length
+        collection<<yield(array[i])
+        i+=1
+end
+array.collect do |language|
+end
+array.collect do |names|
+    "#{names.split(" ").first}"
+end
+
+return collection
+end
 
